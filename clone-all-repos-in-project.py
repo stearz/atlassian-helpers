eb31ef3145a5b3bfcf5e15d2df65e5ff524fc23b
Stephan Schwarz <stearz@gmx.de>
2021-07-06 12:30:00 +0200

    added argument validation

--- clone-all-repos-in-project.py
+++ clone-all-repos-in-project.py
@@ -7,28 +7,48 @@ import os
 import requests
 from git import Repo
 import argparse
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument('-u', '--url', required=True,
-    help="[required] - The url of your bitbucket server (e.g. https://bitbucket.example.com)")
-parser.add_argument('-l', '--username', required=True,
-    help="[required] - The useranme to login to your bitbucket server")
-parser.add_argument('-p', '--project', required=True,
-    help="[required] - The project to clone the repositories from")
-parser.add_argument('-b', '--basedir', required=False,
-    help="[optional] - The basedir where the repositories should be cloned into")
-
-args = parser.parse_args()
+import validators
 
 # variables
 restApiEndpoint = '/rest/api/1.0/projects/'
 
-# asking for credentials
-password = getpass.getpass("Password for " + args.username + ": ")
+# parse arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-u', '--url', required=True,
+    help="The url of your bitbucket server (e.g. https://bitbucket.example.com)")
+parser.add_argument('-l', '--username', required=True,
+    help="The useranme to login to your bitbucket server")
+parser.add_argument('-p', '--project', required=True,
+    help="The project to clone the repositories from")
+parser.add_argument('-b', '--basedir', required=True,
+    help="The basedir where the repositories should be cloned into")
+parser.add_argument('--noverify', action='store_true', required=False, 
+    help="[optional] Disable SSL verfication by setting this parameter")
+
+args = parser.parse_args()
+
+# sanitizing arguments
+url = args.url.rstrip("/")
+username = args.username
+project = args.project
+basedir = args.basedir.rstrip("/")
+verifyssl = False if args.noverify else True
+
+# checking validity of variables set by arguments
+if (not validators.url(url)):
+    print ('ERROR: %s is no valid url', url)
+    exit(1)
+
+if (not os.path.isdir(basedir)):
+    print ('ERROR: %s is no directory', basedir)
+    exit(1)
+
+# DIALOG - asking user for credentials
+password = getpass.getpass("Password for " + username + ": ")
 
 # getting list of repositories from bitbucket server
-r = requests.get(args.url + restApiEndpoint + args.project + '/repos/?limit=1000000', auth=(args.username, password))
+r = requests.get(url + restApiEndpoint + project + '/repos/?limit=1000000', auth=(username, password), verify=verifyssl)
 if (r.status_code != 200):
     print ('Error contacting Bitbucket: Got status code:', r.status_code)
     exit(2)
@@ -49,12 +69,12 @@ for value in myObject['values']:
     if (gitCloneUrl != ''):
         try:
             # cloning from origin
-            os.makedirs(args.basedir + '/' + gitRepoName)
-            print ('[cloning] ' + args.basedir + '/' + gitRepoName)
-            repo = Repo.clone_from(gitCloneUrl, args.basedir + '/' + gitRepoName)
+            os.makedirs(basedir + '/' + gitRepoName)
+            print ('[cloning] ' + basedir + '/' + gitRepoName)
+            repo = Repo.clone_from(gitCloneUrl, basedir + '/' + gitRepoName)
 
         except OSError:
-            if not os.path.isdir(args.basedir + '/' + gitRepoName):
+            if not os.path.isdir(basedir + '/' + gitRepoName):
                 raise
             else:
-                print ('[ok]      ' + args.basedir + '/' + gitRepoName)
+                print ('[ok]      ' + basedir + '/' + gitRepoName)
